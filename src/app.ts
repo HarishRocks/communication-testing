c8eb1d9c0ccacd34141e1571ebdf89aa13e2b7a1
Gaurav Agarwal <gauravggg21@gmail.com>
2020-07-04 20:13:11 +0530

    minor changes

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,6 @@
 // ToDo ask suraj sir about the added_coins in recievetransaction flow
 import cli_tool from './cli';
 
-
 import { recieveCommand, recieveData } from './communication/recieveData';
 import { sendData } from './communication/sendData';
 
@@ -10,11 +9,20 @@ import * as addCoin from './flows/addCoin';
 import * as sendTransaction from './flows/sendTransaction';
 import * as recieveTransaction from './flows/recieveTransaction';
 import * as wallet from './flows/wallet';
+import { createPort } from './communication/port';
 
-if(process.env.NODE_ENV!.trim() === "cli"){
-
- cli_tool()
-  .catch((err) => console.log(err));
+if (process.env.NODE_ENV!.trim() === 'cli') {
+  cli_tool().catch((err) => console.log(err));
 }
 
-export {sendData, recieveCommand, recieveData, addWallet, addCoin, sendTransaction, recieveTransaction, wallet };
+export {
+  sendData,
+  recieveCommand,
+  recieveData,
+  addWallet,
+  addCoin,
+  sendTransaction,
+  recieveTransaction,
+  wallet,
+  createPort
+};
--- src/cli.ts
+++ src/cli.ts
@@ -17,82 +17,81 @@ import { sendData } from './communication/sendData';
 const log = logs.createSimpleFileLogger('project.log');
 
 const cli_tool = async () => {
-    let selection = await query_list([
-        'Select Wallet',
-        'Add Wallet',
-        'Card Authentication',
-        'Custom',
-    ]);
+  let selection = await query_list([
+    'Select Wallet',
+    'Add Wallet',
+    'Card Authentication',
+    'Custom',
+  ]);
 
-    switch (selection) {
-        case 'Add Wallet':
-        log.info('Add Wallet selected');
-        await addWallet();
-        log.info('Add wallet completed');
-        break;
-        case 'Card Authentication':
-        await cardAuth();
-        break;
-        case 'Select Wallet':
-        log.info('Selecting Wallet');
-        const wallet_id = await query_list(
-            await allWalletsList(),
-            'Select your wallet'
-        );
-        log.info('Selected Wallet ID: ' + wallet_id);
+  switch (selection) {
+    case 'Add Wallet':
+      log.info('Add Wallet selected');
+      await addWallet();
+      log.info('Add wallet completed');
+      break;
+    case 'Card Authentication':
+      await cardAuth();
+      break;
+    case 'Select Wallet':
+      log.info('Selecting Wallet');
+      const wallet_id = await query_list(
+        await allWalletsList(),
+        'Select your wallet'
+      );
+      log.info('Selected Wallet ID: ' + wallet_id);
 
-        selection = await query_list([
-            'Add Coin',
-            'Send Transaction',
-            'Recieve Transaction',
-        ]);
+      selection = await query_list([
+        'Add Coin',
+        'Send Transaction',
+        'Recieve Transaction',
+      ]);
 
-        switch (selection) {
-            case 'Add Coin':
-            log.info('Add coin initiated');
-            await addCoin(wallet_id, undefined);
-            log.info('Add coin finished');
-            break;
-            case 'Send Transaction':
-            log.info('Send transaction initiated');
-            await sendTransaction(wallet_id, undefined, undefined, undefined);
-            log.info('Send transaction finished');
-            break;
+      switch (selection) {
+        case 'Add Coin':
+          log.info('Add coin initiated');
+          await addCoin(wallet_id, undefined);
+          log.info('Add coin finished');
+          break;
+        case 'Send Transaction':
+          log.info('Send transaction initiated');
+          await sendTransaction(wallet_id, undefined, undefined, undefined);
+          log.info('Send transaction finished');
+          break;
 
-            case 'Recieve Transaction':
-            log.info('Recieve transaction initiated');
-            await recieveTransaction(wallet_id, undefined);
-            log.info('Recieve transaction finished');
+        case 'Recieve Transaction':
+          log.info('Recieve transaction initiated');
+          await recieveTransaction(wallet_id, undefined);
+          log.info('Recieve transaction finished');
 
-            break;
-        }
-        break;
-        case 'Custom':
-        const actions = [
-            {
-            type: 'SEND',
-            command: 70,
-            data: '00',
-            },
-            {
-            type: 'RECEIVE',
-            command: 13,
-            },
-            {
-            type: 'SEND',
-            command: 16,
-            data: '12345678',
-            },
-            {
-            type: 'RECEIVE',
-            command: 17,
-            },
-        ];
-        await customAction(actions);
-        break;
-    }
+          break;
+      }
+      break;
+    case 'Custom':
+      const actions = [
+        {
+          type: 'SEND',
+          command: 70,
+          data: '00',
+        },
+        {
+          type: 'RECEIVE',
+          command: 13,
+        },
+        {
+          type: 'SEND',
+          command: 16,
+          data: '12345678',
+        },
+        {
+          type: 'RECEIVE',
+          command: 17,
+        },
+      ];
+      await customAction(actions);
+      break;
+  }
 };
-
 
 // provision();
 // deviceAuthandUpgrade();
