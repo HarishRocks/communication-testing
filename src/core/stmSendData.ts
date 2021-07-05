28f917060d524362074fa3af50140420af22c553
Md Irshad Ansari <irshadjsr21@gmail.com>
2021-07-05 16:21:42 +0530

    📦 NEW: STM: Error codes on update and improve header in bin

--- src/core/stmSendData.ts
+++ src/core/stmSendData.ts
@@ -9,12 +9,38 @@ const log = logs.createSimpleFileLogger('project.log');
 
 const { START_OF_FRAME } = constants;
 const ACK_PACKET = '06';
+const ERROR_CODES = [
+  {
+    code: '07',
+    message: 'Limit exceeded',
+  },
+  {
+    code: '08',
+    message: 'Wrong firmware version',
+  },
+  {
+    code: '09',
+    message: 'Wrong hardware version',
+  },
+  {
+    code: '0a',
+    message: 'Wrong magic number',
+  },
+  {
+    code: '0b',
+    message: 'Signature not verified',
+  },
+];
 
+/*
+ * Resolves to an error msg returned from device or undefined if successful,
+ * throws error if unable to send packet.
+ */
 const writePacket = (
   connection: any,
   packet: any,
   options?: { timeout?: number }
-) => {
+): Promise<string | undefined> => {
   return new Promise((resolve, reject) => {
     /**
      * Ensure is listener is activated first before writing
@@ -23,12 +49,22 @@ const writePacket = (
       const ePacketData = ePacket.toString('hex');
       console.log(ePacketData);
       // console.log(data);
+
+      // When a error code is received, return the error
+      for (const errorCode of ERROR_CODES) {
+        if (ePacketData.includes(errorCode.code)) {
+          resolve(errorCode.message);
+          connection.removeListener('data', eListener);
+          return;
+        }
+      }
+
       if (ePacketData.includes(ACK_PACKET)) {
         log.info('ack recieved');
         /**
          * We got a packet so just accept
          */
-        resolve(true);
+        resolve(undefined);
         connection.removeListener('data', eListener);
       }
     };
@@ -67,17 +103,24 @@ const sendData = async (connection: any, data: string) => {
       let tries = 1;
       while (tries <= 5) {
         try {
-          await writePacket(
+          const errorMsg = await writePacket(
             connection,
             d,
             // Wait for 10 sec for the 1st packet ACK, there may be heavy processing task
             // in device after 1st packet.
             index === 0 ? { timeout: 10000 } : undefined
           );
-          i += 1;
-          console.log('Send Packet: ' + d);
-          resolve(true);
-          return;
+          if (!errorMsg) {
+            i += 1;
+            console.log('Send Packet: ' + d);
+            resolve(true);
+            return;
+          } else {
+            log.error(errorMsg);
+            console.log(errorMsg);
+            reject(false);
+            return;
+          }
         } catch (e) {
           log.error('Caught error');
           console.log('Caught Error');
