b5dd951b94a9e7a7ffe67dc1c553f5718817738c
Md Irshad Ansari <irshadjsr21@gmail.com>
2021-09-25 17:34:09 +0530

    👌 IMPROVE: .gitignore

--- src/flows/custom.ts
+++ src/flows/custom.ts
@@ -7,7 +7,7 @@ export default async (actions: any[]) => {
   const { connection, serial } = await createPort();
   console.log('Serial Number: ' + serial);
   await new Promise((resolve, reject) =>
-    connection.open(err => {
+    connection.open((err) => {
       if (err) {
         reject(err);
       } else {
