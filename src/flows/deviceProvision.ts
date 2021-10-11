55c8e643d5d558fb393b0736c592018557b14d45
Md Irshad Ansari <irshadjsr21@gmail.com>
2021-10-12 03:54:14 +0530

    👌 IMPROVE: Added new server routes to provision

--- src/flows/deviceProvision.ts
+++ src/flows/deviceProvision.ts
@@ -8,18 +8,33 @@ import { receiveCommand } from '../core/recieveData';
 import { getKeysFromSeed, calculatePathFromIndex } from '../utils/crypto';
 import { intToUintByte } from '../bytes';
 
-const cyBaseURL =
-  'http://cypherockserver-env.eba-hvatxy8g.ap-south-1.elasticbeanstalk.com';
+const cyBaseURL = 'https://api.cypherock.com';
 
 const sleep = (ms: any) => {
-  return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise((resolve) => setTimeout(resolve, ms));
+};
+
+const getProvisionIndex = async (serial: string) => {
+  const res = await axios.post(`${cyBaseURL}/provision/index`, {
+    type: 'DEVICE',
+    serial,
+  });
+
+  console.log('Response from server:');
+  console.log(res.data);
+
+  if (res.data.status !== 1) {
+    throw new Error('Server responded in error');
+  }
+
+  return res.data.data.index;
 };
 
 const provisionDevice = async (serial: string, publicKey: string) => {
-  const res = await axios.post(`${cyBaseURL}/provision/add`, {
+  const res = await axios.post(`${cyBaseURL}/provision/index/add`, {
     type: 'DEVICE',
     serial,
-    publicKey
+    publicKey,
   });
 
   console.log('Response from server:');
@@ -30,81 +45,96 @@ const provisionDevice = async (serial: string, publicKey: string) => {
   }
 };
 
-let index = '0';
+const confirmProvisonDevice = async (serial: string) => {
+  const res = await axios.post(`${cyBaseURL}/provision/index/confirm`, {
+    type: 'DEVICE',
+    serial,
+  });
+
+  console.log('Response from server:');
+  console.log(res.data);
+
+  if (res.data.status !== 1) {
+    throw new Error('Server responded in error');
+  }
+};
 
 const deviceProvision = async () => {
   const { connection } = await createPort();
-  await openConnection(connection);
-  const date = new Date();
+  try {
+    await openConnection(connection);
+    const date = new Date();
 
-  const day = date.getDate().toString(16);
-  const month = (date.getMonth() + 1).toString(16);
-  const year = date.getFullYear().toString(16);
+    const day = date.getDate().toString(16);
+    const month = (date.getMonth() + 1).toString(16);
+    const year = date.getFullYear().toString(16);
 
-  const datePayload =
-    day.padStart(2, '0') + month.padStart(2, '0') + year.padStart(4, '0');
+    const datePayload =
+      day.padStart(2, '0') + month.padStart(2, '0') + year.padStart(4, '0');
 
-  await sendData(connection, 84, '01' + datePayload); // Add date: DMY
+    await sendData(connection, 84, '01' + datePayload); // Add date: DMY
 
-  const serialAndKey: string = await receiveCommand(connection, 82);
-  const serialNumber = serialAndKey.slice(0, 64).toUpperCase();
+    const serialAndKey: string = await receiveCommand(connection, 82);
+    const serialNumber = serialAndKey.slice(0, 64).toUpperCase();
 
-  if (index === undefined || index === null) {
-    console.log('Upload failed. Try again.\nExiting function..');
-    return 0;
-  }
+    console.log('From Device: Serial:');
+    console.log({ serialNumber });
+    if (serialNumber.search(/[^0]/) === -1) {
+      throw new Error('Device returned invalid serial or public key');
+    }
 
-  const deviceNfcKeys = await getKeysFromSeed(
-    config.SECRET_SEED,
-    `m/1000'/1'/2'/0/${calculatePathFromIndex(index)}`,
-    'nist256p1'
-  );
-  const cardNfcKeys = await getKeysFromSeed(
-    config.SECRET_SEED,
-    `m/1000'/0'/2'/0`,
-    'nist256p1'
-  );
-  const deviceAuthKeys = await getKeysFromSeed(
-    config.SECRET_SEED,
-    `m/1000'/1'/0'/0/${calculatePathFromIndex(index)}`,
-    'nist256p1'
-  );
+    const index = await getProvisionIndex(serialNumber);
 
-  console.log('From Device: Serial:');
-  console.log({ serialNumber });
-  if (serialNumber.search(/[^0]/) === -1) {
-    throw new Error('Device returned invalid serial or public key');
-  }
+    if (index === undefined || index === null) {
+      console.log('Invalid index from server: ', index);
+      return 0;
+    }
 
-  const keysData =
-    deviceAuthKeys.privateKey +
-    deviceAuthKeys.publicKey +
-    intToUintByte(index, 8 * 8) + // 8 Bytes index
-    deviceNfcKeys.privateKey +
-    cardNfcKeys.xpub;
+    const deviceNfcKeys = await getKeysFromSeed(
+      config.SECRET_SEED,
+      `m/1000'/1'/2'/0/${calculatePathFromIndex(index)}`,
+      'nist256p1'
+    );
+    const cardNfcKeys = await getKeysFromSeed(
+      config.SECRET_SEED,
+      `m/1000'/0'/2'/0`,
+      'nist256p1'
+    );
+    const deviceAuthKeys = await getKeysFromSeed(
+      config.SECRET_SEED,
+      `m/1000'/1'/0'/0/${calculatePathFromIndex(index)}`,
+      'nist256p1'
+    );
 
-  await sendData(connection, 84, '02' + keysData); // Add date: DMY
-  await sleep(200);
-  const isSuccess = await receiveCommand(connection, 84);
+    const keysData =
+      deviceAuthKeys.privateKey +
+      deviceAuthKeys.publicKey +
+      intToUintByte(index, 8 * 8) + // 8 Bytes index
+      deviceNfcKeys.privateKey +
+      cardNfcKeys.xpub;
 
-  if (isSuccess.startsWith('01')) {
+    await provisionDevice(serialNumber, deviceAuthKeys.publicKey);
+
+    await sendData(connection, 84, '02' + keysData); // Add date: DMY
+    await sleep(200);
+
+    const isSuccess = await receiveCommand(connection, 84);
+
+    if (!isSuccess.startsWith('01')) {
+      throw new Error('Failed to provision device.');
+    }
+
+    await confirmProvisonDevice(serialNumber);
     console.log('Device provisioned successfully.');
-  } else {
-    console.log('Failed to provision device.');
-  }
 
-  //try {
-  //// await provisionDevice(serialNumber, publicKey);
-  //await sleep(2000);
-  //await sendData(connection, 81, '02');
-  //} catch (error) {
-  //await sleep(2000);
-  //await sendData(connection, 81, '03');
-  //console.log(error);
-  //} finally {
-  //await closeConnection(connection);
-  //console.log('close');
-  //}
+    await sleep(2000);
+  } catch (error) {
+    console.log(error);
+    await sleep(2000);
+  } finally {
+    await closeConnection(connection);
+    console.log('close');
+  }
 };
 
 export default deviceProvision;
